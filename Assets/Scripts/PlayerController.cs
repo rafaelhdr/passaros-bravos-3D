95569cd5d648e0dd8dad83fd1c57c378cd6e1de4
Rafael <rafael@hurpia.com.br>
2016-06-03 18:46:59 -0300

    Lançamento oblíquo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,16 @@ public class PlayerController : MonoBehaviour {
 	public Rigidbody rb;
 
 	public float speed;
+	public float elasticSpeed;
 	public int status;
 	public float forceX;
 	public float forceY;
+	protected Vector3 position0;
 
     void Start() 
     {
         rb = GetComponent<Rigidbody>();
+		rb.useGravity = false;
 
 		// Doing nothing
 		status = 0;
@@ -26,6 +29,7 @@ public class PlayerController : MonoBehaviour {
 
 			if (moveSubmit == 1.0)
 			{
+				position0 = rb.position;
 				status = 1;
 			}
 		}
@@ -37,8 +41,14 @@ public class PlayerController : MonoBehaviour {
 			float moveMouseX = Input.GetAxis("Mouse X");
 			float moveMouseY = Input.GetAxis("Mouse Y");
 
+			float yPull = moveMouseX + moveMouseY;
+
 			forceX = forceX + moveMouseX;
 			forceY = forceY + moveMouseY;
+
+			Vector3 position;
+			position = rb.position;
+			rb.position = rb.position + new Vector3(moveMouseX * elasticSpeed, yPull * elasticSpeed, moveMouseY * elasticSpeed);
 
 			if (moveSubmit == 0.0)
 			{
@@ -49,7 +59,10 @@ public class PlayerController : MonoBehaviour {
 		// Throw
 		if (status == 2)
 		{
-			Vector3 movement = new Vector3 (-forceX * speed, 100.0f, -forceY * speed);
+			float yForce = forceX + forceY;
+			// Vector3 movement = new Vector3 (-forceX * speed, -yForce * speed, -forceY * speed);
+			Vector3 movement = (position0 - rb.position) * speed;
+			rb.useGravity = true;
 			rb.AddForce(movement);
 
 			status = 3;
